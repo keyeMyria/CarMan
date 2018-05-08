466179cea53018a448facb8259891b8846f94c90
kovrizhkin_v <vovanychkov95@gmail.com>
2018-05-08 22:50:45 +0300

    first screen created

---
+++ Containers/AddExpenseScreen/AddExpenseScreen.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import { Text, View} from 'react-native'
+
+export default class AddExpenceScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text>AddExpenceScreen</Text>
+            </View>
+        )
+    }
+}
---
+++ Containers/AddExpenseScreen/index.js
@@ -0,0 +1,3 @@
+import AddExpenseScreen from './AddExpenseScreen';
+
+export default AddExpenseScreen;
